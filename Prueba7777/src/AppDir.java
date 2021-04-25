72906ee82ee938e1f77d69f468a701854558ba22
julian <julian@192.168.0.12>
2021-04-25 12:19:52 +0200

    dir sin ruta, dir ruta valida

--- Prueba7777/src/AppDir.java
+++ Prueba7777/src/AppDir.java
@@ -5,14 +5,14 @@ public class AppDir {
 	public static void main(String[] args) {
 		// TODO Esbozo de m�todo generado autom�ticamente
 		//dir sin especificar ruta: en la carpeta del proyecto:
-				Dir dirSinRuta = new Dir();
-				dirSinRuta.generarArchivos();
-				dirSinRuta.mostrar();
-				
-				//dir especificando una ruta valida:
-				Dir dirRutaValida = new Dir("C:/Users/tarde/Desktop");
-				dirRutaValida.generarArchivos();
-				dirRutaValida.mostrar();
+		Dir dirSinRuta = new Dir();
+		dirSinRuta.generarArchivos();
+		dirSinRuta.mostrar();
+		
+		//dir especificando una ruta valida:
+		Dir dirRutaValida = new Dir("C:/Users/tarde/Desktop");
+		dirRutaValida.generarArchivos();
+		dirRutaValida.mostrar();
 				
 	}
 
