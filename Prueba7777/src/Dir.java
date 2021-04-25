a68910c2a209b7f1a560b55ddcbc18e14b78fbb3
julian <julian@192.168.0.12>
2021-04-25 11:53:08 +0200

    variables clase y constructores

--- Prueba7777/src/Dir.java
+++ Prueba7777/src/Dir.java
@@ -1,4 +1,27 @@
+import java.io.File;
 
 public class Dir {
-
+	private String directorio;
+	private File carpeta;
+	private File[] archivos;
+	
+	public Dir() {
+		carpeta = new File(".");
+		directorio = ".";
+	}
+	public Dir(String directorio) {
+		this.directorio = directorio;
+		if (longitudDirectorio()) {
+			carpeta = new File(this.directorio.replace('/', File.separatorChar));
+			if (!carpeta.exists()) {
+				System.out.println("Ruta no valida, se asigna la del directorio actual.");
+				System.out.println("");
+				carpeta = new File(".");
+				this.directorio = ".";
+			}
+		}else {
+			carpeta = new File(".");
+			this.directorio = ".";
+		}
+	}
 }
